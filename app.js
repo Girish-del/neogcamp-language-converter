585c2d6add020d4528c90c3ac0876cf07eafbf3a
Girish-del <72304710+Girish-del@users.noreply.github.com>
2021-03-16 00:37:39 +0530

    updated till clicked

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-var btnTranslate = document.querySelector{"#btn-translate"}
+var btnTranslate = document.querySelector("#btn-translate");
 
-console.log{btnTranslate}
+
+
+btnTranslate.addEventListener("click", function clickEventHandler() {
+    console.log("clicked!")
+})
+
