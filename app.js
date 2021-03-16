d41ee73f30e97caeb1e44a05a8fdf90290fb2e8e
Girish-del <72304710+Girish-del@users.noreply.github.com>
2021-03-17 00:02:06 +0530

    created 2 blocks

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputdiv = document.querySelector("#output");
+
+function clickhandler(){
+    outputdiv.innerText = "translated " + txtInput.value;
+};
 
 
 
-btnTranslate.addEventListener("click", function clickEventHandler() {
-    console.log("clicked!")
-})
+btnTranslate.addEventListener("click", clickhandler)
 
